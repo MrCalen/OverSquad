eb0b4b4c3af8423d5c361a395f702a8804e1aece
Corentin Brauge <brauge_c@epita.fr>
2016-07-05 16:17:37 +0200

    tests: improve room tests with user leaving

--- tests/PlayersManagerTest.php
+++ tests/PlayersManagerTest.php
@@ -58,12 +58,19 @@ class PlayersManagerTest extends TestCase
         $conn = new Connection(null);
         $conn->setUser(new User());
         $conns = array();
-        for ($i = 0; $i < 51; $i++)
+        for ($i = 0; $i < 50; $i++)
         {
             $conn = new Connection(null);
+            $user = new User();
+            $user->setCreatedAt($i);
             $conn->setUser(new User());
             array_push($conns, $conn);
         }
+
+        $this->assertTrue(count($pm->getRooms()) == 0);
+        $pm->userConnected($conns[0], [Roles::ATTACK]);
+        $pm->userLeave($conns[0]);
+        $this->assertTrue(count($pm->getRooms()) == 0);
         $pm->userConnected($conns[0], [Roles::ATTACK]);
         $pm->userConnected($conns[1], [Roles::TANK]);
         $pm->userConnected($conns[2], [Roles::SUPPORT]);
@@ -84,17 +91,84 @@ class PlayersManagerTest extends TestCase
             $pm->userConnected($conns[$i], [Roles::DEFENSE]);
             $this->assertTrue(count($pm->getRooms()) == 8);
         }
-        for ($i = 26; $i <= 46; $i++)
+        for ($i = 26; $i <= 45; $i++)
         {
             $pm->userConnected($conns[$i], [Roles::ATTACK]);
-            $i++;
             $this->assertTrue(count($pm->getRooms()) == 8);
         }
-        $pm->userConnected($conns[47], [Roles::ATTACK]);
-        $pm->userConnected($conns[48], [Roles::TANK]);
-        $pm->userConnected($conns[49], [Roles::SUPPORT]);
-        $pm->userConnected($conns[50], [Roles::DEFENSE]);
+        $pm->userConnected($conns[46], [Roles::ATTACK]);
+        $pm->userConnected($conns[47], [Roles::TANK]);
+        $pm->userConnected($conns[48], [Roles::SUPPORT]);
+        $pm->userConnected($conns[49], [Roles::DEFENSE]);
         $this->assertTrue(count($pm->getRooms()) == 9);
+
+        // leave
+        for ($i = 0; $i <= 4; $i++)
+        {
+            $pm->userLeave($conns[$i]);
+        }
+        $pm->userLeave($conns[14]);
+
+        $this->assertTrue(count($pm->getRooms()) == 8);
+
+        $pm->userConnected($conns[0], [Roles::ATTACK]);
+        $pm->userConnected($conns[1], [Roles::TANK]);
+        $pm->userConnected($conns[2], [Roles::SUPPORT]);
+        $pm->userConnected($conns[3], [Roles::ATTACK]);
+        $pm->userConnected($conns[4], [Roles::ATTACK]);
+        $pm->userConnected($conns[14], [Roles::DEFENSE]);
+
+        $this->assertTrue(count($pm->getRooms()) == 9);
+
+        // make all attack leave
+        $pm->userLeave($conns[0]);
+        $pm->userLeave($conns[3]);
+        $pm->userLeave($conns[4]);
+        $pm->userLeave($conns[5]);
+        for ($i = 26; $i <= 45; $i++)
+        {
+            $pm->userLeave($conns[$i]);
+        }
+
+        $pm->userLeave($conns[46]);
+        $this->assertTrue(count($pm->getRooms()) == 9);
+
+        // leave support
+        for ($i = 6; $i <= 12; $i++)
+        {
+            $pm->userLeave($conns[$i]);
+        }
+        $this->assertTrue(count($pm->getRooms()) == 9);
+
+        $pm->userLeave($conns[1]);
+        $pm->userLeave($conns[2]);
+
+        for ($i = 13; $i <= 25; $i++)
+        {
+            $pm->userLeave($conns[$i]);
+            $i++;
+            $pm->userLeave($conns[$i]);
+
+        }
+
+        $this->assertTrue(count($pm->getRooms()) == 3);
+
+        $pm->userLeave($conns[46]);
+        $pm->userLeave($conns[47]);
+        $pm->userLeave($conns[48]);
+        $pm->userLeave($conns[49]);
+
+        $this->assertTrue(count($pm->getRooms()) == 0);
+
+        // force leave
+        for ($i = 0; $i < 50; $i++) {
+            $pm->userLeave($conns[$i]);
+        }
+
+        // Should be 0
+        $this->assertTrue(count($pm->getRooms()) == 0);
+
+
     }
 
     /**
