f732380de311eeb6348359ab4375278319e9101c
Nils Layet <nils@epita.fr>
2016-07-05 21:05:55 +0200

    docker: add docker for deployment

---
+++ Dockerfile
@@ -0,0 +1,88 @@
+FROM alpine:3.4
+
+ENV php_conf /etc/php5/php.ini 
+ENV fpm_conf /etc/php5/php-fpm.conf
+
+RUN apk add --no-cache bash \
+    openssh-client \
+    wget \
+    nginx \
+    supervisor \
+    curl \
+    git \
+    mysql \
+    mysql-client \
+    php5-fpm \
+    php5-pdo \
+    php5-pdo_mysql \
+    php5-mysql \
+    php5-mysqli \
+    php5-mcrypt \
+    php5-ctype \
+    php5-zlib \
+    php5-gd \
+    php5-intl \
+    php5-memcache \
+    php5-sqlite3 \
+    php5-pgsql \
+    php5-xml \
+    php5-xsl \
+    php5-curl \
+    php5-openssl \
+    php5-iconv \
+    php5-json \
+    php5-phar \
+    php5-soap \
+    php5-dom
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
+RUN mkdir -p /etc/nginx && \
+    mkdir -p /var/www/app && \
+    mkdir -p /run/nginx && \
+    mkdir -p /var/log/supervisor 
+
+ADD docker-config/supervisord.conf /etc/supervisord.conf
+
+RUN rm -Rf /etc/nginx/nginx.conf
+ADD docker-config/nginx.conf /etc/nginx/nginx.conf
+
+RUN mkdir -p /etc/nginx/sites-available/ && \
+mkdir -p /etc/nginx/sites-enabled/ && \
+mkdir -p /etc/nginx/ssl/ && \
+rm -Rf /var/www/* && \
+mkdir /var/www/html/
+ADD docker-config/nginx-site.conf /etc/nginx/sites-available/default.conf
+RUN ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
+
+RUN sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" ${php_conf} && \
+sed -i -e "s/upload_max_filesize\s*=\s*2M/upload_max_filesize = 100M/g" ${php_conf} && \
+sed -i -e "s/post_max_size\s*=\s*8M/post_max_size = 100M/g" ${php_conf} && \
+sed -i -e "s/variables_order = \"GPCS\"/variables_order = \"EGPCS\"/g" ${php_conf} && \
+sed -i -e "s/;daemonize\s*=\s*yes/daemonize = no/g" ${fpm_conf} && \
+sed -i -e "s/;catch_workers_output\s*=\s*yes/catch_workers_output = yes/g" ${fpm_conf} && \
+sed -i -e "s/pm.max_children = 4/pm.max_children = 4/g" ${fpm_conf} && \
+sed -i -e "s/pm.start_servers = 2/pm.start_servers = 3/g" ${fpm_conf} && \
+sed -i -e "s/pm.min_spare_servers = 1/pm.min_spare_servers = 2/g" ${fpm_conf} && \
+sed -i -e "s/pm.max_spare_servers = 3/pm.max_spare_servers = 4/g" ${fpm_conf} && \
+sed -i -e "s/pm.max_requests = 500/pm.max_requests = 200/g" ${fpm_conf} && \
+sed -i -e "s/user = nobody/user = nginx/g" ${fpm_conf} && \
+sed -i -e "s/group = nobody/group = nginx/g" ${fpm_conf} && \
+sed -i -e "s/;listen.mode = 0660/listen.mode = 0666/g" ${fpm_conf} && \
+sed -i -e "s/;listen.owner = nobody/listen.owner = nginx/g" ${fpm_conf} && \
+sed -i -e "s/;listen.group = nobody/listen.group = nginx/g" ${fpm_conf} && \
+sed -i -e "s/listen = 127.0.0.1:9000/listen = \/var\/run\/php-fpm.sock/g" ${fpm_conf} &&\
+ln -s /etc/php5/php.ini /etc/php5/conf.d/php.ini && \
+find /etc/php5/conf.d/ -name "*.ini" -exec sed -i -re 's/^(\s*)#(.*)/\1;\2/g' {} \;
+
+COPY docker-config/my.cnf /etc/mysql/my.cnf
+
+ADD OverSquad /var/www/
+RUN cd /var/www && composer install
+RUN chmod 777 -R /var/www/storage /var/www/bootstrap/cache
+
+ADD docker-config/start.sh /start.sh
+RUN chmod 755 /start.sh
+
+EXPOSE 443 80
+
+CMD ["/start.sh"]
---
+++ docker-config/start.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# Disable Strict Host checking for non interactive git clones
+
+mkdir -p -m 0700 /root/.ssh
+echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
+
+if [ ! -z "$SSH_KEY" ]; then
+ echo $SSH_KEY > /root/.ssh/id_rsa.base64
+ base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
+ chmod 600 /root/.ssh/id_rsa
+fi
+
+# Set custom webroot
+if [ ! -z "$WEBROOT" ]; then
+ sed -i "s#root /var/www/html;#root ${WEBROOT};#g" /etc/nginx/sites-available/default.conf
+fi
+
+# Setup git variables
+if [ ! -z "$GIT_EMAIL" ]; then
+ git config --global user.email "$GIT_EMAIL"
+fi
+if [ ! -z "$GIT_NAME" ]; then
+ git config --global user.name "$GIT_NAME"
+ git config --global push.default simple
+fi
+
+# Dont pull code down if the .git folder exists
+if [ ! -d "/var/www/html/.git" ]; then
+ # Pull down code from git for our site!
+ if [ ! -z "$GIT_REPO" ]; then
+   # Remove the test index file
+   rm -Rf /var/www/html/index.php
+   if [ ! -z "$GIT_BRANCH" ]; then
+     git clone -b $GIT_BRANCH $GIT_REPO /var/www/html/
+   else
+     git clone $GIT_REPO /var/www/html/
+   fi
+   chown -Rf nginx.nginx /var/www/html
+ fi
+fi
+
+# Display PHP error's or not
+if [[ "$ERRORS" != "1" ]] ; then
+ echo php_flag[display_errors] = off >> /etc/php5/php-fpm.conf
+else
+ echo php_flag[display_errors] = on >> /etc/php5/php-fpm.conf
+fi
+
+# Very dirty hack to replace variables in code with ENVIRONMENT values
+if [[ "$TEMPLATE_NGINX_HTML" == "1" ]] ; then
+  for i in $(env)
+  do
+    variable=$(echo "$i" | cut -d'=' -f1)
+    value=$(echo "$i" | cut -d'=' -f2)
+    if [[ "$variable" != '%s' ]] ; then
+      replace='\$\$_'${variable}'_\$\$'
+      find /var/www/html -type f -exec sed -i -e 's/'${replace}'/'${value}'/g' {} \;
+    fi
+  done
+fi
+
+# Start mysql daemon
+if [ -d /app/mysql ]; then
+  echo "[i] MySQL directory already present, skipping creation"
+else
+  echo "[i] MySQL data directory not found, creating initial DBs"
+
+  mysql_install_db --user=root > /dev/null
+
+  if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
+    MYSQL_ROOT_PASSWORD=root
+    echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
+  fi
+
+  MYSQL_DATABASE="oversquad"
+  MYSQL_USER="oversquad"
+  MYSQL_PASSWORD="oversquad"
+
+  if [ ! -d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
+  fi
+
+  tfile=`mktemp`
+  if [ ! -f "$tfile" ]; then
+      return 1
+  fi
+
+  cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost';
+EOF
+
+  if [ "$MYSQL_DATABASE" != "" ]; then
+    echo "[i] Creating database: $MYSQL_DATABASE"
+    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+
+    if [ "$MYSQL_USER" != "" ]; then
+      echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+      echo "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+      echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+      echo "FLUSH PRIVILEGES;" >> $tfile
+    fi
+  fi
+
+  /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
+  rm -f $tfile
+fi
+
+echo "[i] Starting mysql daemon"
+exec /usr/bin/mysqld --user=root &
+sleep 3
+
+BOOTSTRAP_FILE="/var/www/docker-config/bootstrap.sh"
+if [ -f "$BOOTSTRAP_FILE" ]; then
+  echo "[i] Running sh $BOOTSTRAP_FILE"
+  cd /var/www && sh "$BOOTSTRAP_FILE" 
+fi
+
+echo "[i] Starting the websocket server"
+cd /var/www && php artisan w:serve &
+
+# Start supervisord and services
+/usr/bin/supervisord -n -c /etc/supervisord.conf
